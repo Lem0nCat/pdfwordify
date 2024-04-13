04591a8a6eec20576f30a30a5c41114d6f22599c
LemonCat <ilnaz.imamov77@gmail.com>
2024-04-13 18:39:59 +0300

    Удалены ненужные функции

--- main.py
+++ main.py
@@ -10,32 +10,19 @@ FILE_NAME = "Privacy policy"
 PDF_PATH = f"Resources/PDF_files/{FILE_NAME}.pdf"
 WORD_PATH = f"Resources/word_files/{FILE_NAME}.docx"
 
-def get_tree(pages, key='line_format'):
+def get_tree(pages):
     for i, page in enumerate(pages):
         print(f'Page #{i}')
-        for j, element in enumerate(page[key]):
+        for j, element in enumerate(page):
             print(f'\tElement #{j}')
-            if isinstance(element, list):
-                for font in element:
-                    print(f"\t\t{font}")
-            else:
-                print(f'\t\t{element}')
+            
+            print(f'\t\t{type(element)}')
+            
             print()
         print()
 
-def test_font_parse(pages):
-    for page in pages:
-        for element in page['line_format']:
-            if isinstance(element, list):
-                for font in element:
-                    fontname, bold, italic = parse_font_info(font[0])
-                    print(f"Before: {font[0]}\nAfter: {fontname}\nBold: {bold}, Italic: {italic}, Size: {font[1]}\n")
-
-
 content_per_page = extract_all(PDF_PATH)
 write_to_word_v2(content_per_page, WORD_PATH)
-# get_tree(content_per_page, "text_from_images")
+# get_tree(content_per_page)
+# print(content_per_page[0][7])
 
-# table = extract_table(PDF_PATH, 4, 0)
-# for row in table:
-#     print(row)
